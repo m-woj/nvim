be29458c519773375def3842943e146df24b0a12
Mateusz Wojciechowski <mwojciechowski296@gmail.com>
2024-08-06 20:20:29 +0200

    Initial commit

---
+++ lua/plugins/custom.lua
@@ -0,0 +1,37 @@
+return {
+    {
+	"nvim-treesitter/nvim-treesitter",
+    	build = ":TSUpdate",
+    },
+
+    {
+	"nvim-tree/nvim-tree.lua",
+        version = "*",
+        lazy = false,
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            require("nvim-tree").setup {}
+        end,
+	},
+
+    {
+      "folke/which-key.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      },
+      keys = {
+        {
+          "<leader>?",
+          function()
+            require("which-key").show({ global = false })
+          end,
+          desc = "Buffer Local Keymaps (which-key)",
+        },
+      },
+    }
+}
