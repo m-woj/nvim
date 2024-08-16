c3dd1d21bd35ebd4214bcb8bfd29729a3094f402
Mateusz Wojciechowski <mwojciechowski296@gmail.com>
2024-08-17 01:19:23 +0200

    Added xclip

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,4 +1,4 @@
-apt install ripgrep nodejs npm fd-find python3-pip universal-ctags &&\
+apt install ripgrep xclip nodejs npm fd-find python3-pip universal-ctags &&\
     npm install n -g &&\
     n latest &&\
     pip3 install pynvim
