94bcaf6eeb97c68c83591ab3ae5fab8c3948c831
Mateusz Wojciechowski <matwojciechowski@mon.gov.pl>
2024-09-11 13:16:58 +0200

    Added icons to dap

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -11,6 +11,14 @@ return {
         "nvim-neotest/nvim-nio",
     },
     config = function()
+        vim.cmd("highlight Red guifg=#FF0000")
+
+        vim.fn.sign_define("DapBreakpoint", { text = "🛑", texthl = "", linehl = "Red", numhl = "" })
+        vim.fn.sign_define("DapBreakpointCondition", { text = "🔵", texthl = "", linehl = "", numhl = "" })
+        vim.fn.sign_define("DapLogPoint", { text = "📍", texthl = "", linehl = "", numhl = "" })
+        vim.fn.sign_define("DapStopped", { text = "➡️", texthl = "", linehl = "", numhl = "" })
+        vim.fn.sign_define("DapBreakpointRejected", { text = "❌", texthl = "", linehl = "", numhl = "" })
+
         local python_debugpy_path = vim.fn.stdpath("data") .. "/mason/packages/debugpy/venv/bin/python"
         require("dap-python").setup(python_debugpy_path)
         require("overseer").enable_dap()
