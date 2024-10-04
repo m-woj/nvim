acc675f82e1d1ca3f9887095b2923355aff9a27b
Mateusz Wojciechowski <matwojciechowski@mon.gov.pl>
2024-10-04 10:39:30 +0200

    Change tabby config

--- lua/plugins/tabby.lua
+++ lua/plugins/tabby.lua
@@ -3,7 +3,7 @@ return {
     dependencies = "nvim-tree/nvim-web-devicons",
     config = function()
         require("tabby").setup({
-            preset = "active_wins_at_tail",
+            preset = "tab_only",
         })
     end,
 }
