729155378c2bbb51f3a8c6bf768515b3f2941096
Mateusz Wojciechowski <matwojciechowski@mon.gov.pl>
2024-10-14 13:31:13 +0200

    Dap UI not showing automatically

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -23,19 +23,8 @@ return {
         require("dap-python").setup(python_debugpy_path)
         require("overseer").enable_dap()
 
-        require("dapui").setup()
-        local dap, dapui = require("dap"), require("dapui")
-        dap.listeners.before.attach.dapui_config = function()
-            dapui.open()
-        end
-        dap.listeners.before.launch.dapui_config = function()
-            dapui.open()
-        end
-        dap.listeners.before.event_terminated.dapui_config = function()
-            dapui.close()
-        end
-        dap.listeners.before.event_exited.dapui_config = function()
-            dapui.close()
-        end
+        local dapui = require("dapui")
+        dapui.setup()
+        vim.keymap.set("n", "<leader>du", dapui.toggle, { desc = "Toggle DAP UI" })
     end,
 }
