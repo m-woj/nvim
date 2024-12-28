57bfbee52a1e0466cb9fff4c623631d170e0a9e8
Mateusz Wojciechowski <mwojciechowski296@gmail.com>
2024-12-28 15:47:04 +0100

    up

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -2,15 +2,102 @@ return {
     "yetone/avante.nvim",
     event = "VeryLazy",
     lazy = false,
+    enabled = function()
+        return vim.g.avante_provider ~= nil and vim.g.avante_provider ~= ""
+    end,
     opts = {
         provider = vim.g.avante_provider,
         copilot = {
             max_tokens = 16000,
         },
+
+        --- @class AvanteRepoMapConfig
+        repo_map = {
+            ignore_patterns = { "%.git", "%.worktree", "__pycache__", "node_modules", ".venv*", "venv*" }, -- ignore files matching these
+            negate_patterns = {}, -- negate ignore files matching these.
+        },
+
+        windows = {
+            ---@type "right" | "left" | "top" | "bottom"
+            position = "right", -- the position of the sidebar
+            wrap = true, -- similar to vim.o.wrap
+            width = 45, -- default % based on available width
+            sidebar_header = {
+                enabled = true, -- true, false to enable/disable the header
+                align = "center", -- left, center, right for title
+                rounded = true,
+            },
+            input = {
+                prefix = "> ",
+                height = 8, -- Height of the input window in vertical layout
+            },
+            edit = {
+                border = "rounded",
+                start_insert = true, -- Start insert mode when opening the edit window
+            },
+            ask = {
+                floating = false, -- Open the 'AvanteAsk' prompt in a floating window
+                start_insert = true, -- Start insert mode when opening the ask window
+                border = "rounded",
+                ---@type "ours" | "theirs"
+                focus_on_apply = "ours", -- which diff to focus after applying
+            },
+        },
+
+        mappings = {
+            ---@class AvanteConflictMappings
+            diff = {
+                ours = "co",
+                theirs = "ct",
+                all_theirs = "ca",
+                both = "cb",
+                cursor = "cc",
+                next = "]x",
+                prev = "[x",
+            },
+            suggestion = {
+                accept = "<M-l>",
+                next = "<M-]>",
+                prev = "<M-[>",
+                dismiss = "<C-]>",
+            },
+            jump = {
+                next = "]]",
+                prev = "[[",
+            },
+            submit = {
+                normal = "<CR>",
+                insert = "<C-s>",
+            },
+            -- NOTE: The following will be safely set by avante.nvim
+            ask = "<leader>aa",
+            edit = "<leader>ae",
+            refresh = "<leader>ar",
+            focus = "<leader>af",
+            toggle = {
+                default = "<leader>at",
+                debug = "<leader>ad",
+                hint = "<leader>ah",
+                suggestion = "<leader>as",
+                repomap = "<leader>aR",
+            },
+            sidebar = {
+                apply_all = "A",
+                apply_cursor = "a",
+                switch_windows = "<Tab>",
+                reverse_switch_windows = "<S-Tab>",
+            },
+        },
+        keys = {
+            {
+                "<leader>ip",
+                function()
+                    return vim.bo.filetype == "AvanteInput" and require("avante.clipboard").paste_image() or require("img-clip").paste_image()
+                end,
+                desc = "clip: paste image",
+            },
+        },
     },
-    enabled = function()
-        return vim.g.avante_provider ~= nil and vim.g.avante_provider ~= ""
-    end,
     build = "make",
     dependencies = {
         "nvim-treesitter/nvim-treesitter",
@@ -42,59 +129,6 @@ return {
                 file_types = { "markdown", "Avante" },
             },
             ft = { "markdown", "Avante" },
-        },
-    },
-    mappings = {
-        ---@class AvanteConflictMappings
-        diff = {
-            ours = "co",
-            theirs = "ct",
-            all_theirs = "ca",
-            both = "cb",
-            cursor = "cc",
-            next = "]x",
-            prev = "[x",
-        },
-        suggestion = {
-            accept = "<M-l>",
-            next = "<M-]>",
-            prev = "<M-[>",
-            dismiss = "<C-]>",
-        },
-        jump = {
-            next = "]]",
-            prev = "[[",
-        },
-        submit = {
-            normal = "<CR>",
-            insert = "<C-s>",
-        },
-        -- NOTE: The following will be safely set by avante.nvim
-        ask = "<leader>aa",
-        edit = "<leader>ae",
-        refresh = "<leader>ar",
-        focus = "<leader>af",
-        toggle = {
-            default = "<leader>at",
-            debug = "<leader>ad",
-            hint = "<leader>ah",
-            suggestion = "<leader>as",
-            repomap = "<leader>aR",
-        },
-        sidebar = {
-            apply_all = "A",
-            apply_cursor = "a",
-            switch_windows = "<Tab>",
-            reverse_switch_windows = "<S-Tab>",
-        },
-    },
-    keys = {
-        {
-            "<leader>ip",
-            function()
-                return vim.bo.filetype == "AvanteInput" and require("avante.clipboard").paste_image() or require("img-clip").paste_image()
-            end,
-            desc = "clip: paste image",
         },
     },
 }
--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -19,26 +19,21 @@ return {
         -- see the "default configuration" section below for full documentation on how to define
         -- your own keymap.
         keymap = { preset = "enter", cmdline = { preset = "super-tab" } },
-
+        completion = {
+            list = {
+                selection = function(ctx)
+                    return ctx.mode == "cmdline" and "auto_insert" or "preselect"
+                end,
+            },
+        },
         appearance = {
-            -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-            -- Useful for when your theme doesn't support blink.cmp
-            -- will be removed in a future release
-            use_nvim_cmp_as_default = true,
-            -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-            -- Adjusts spacing to ensure icons are aligned
             nerd_font_variant = "mono",
         },
 
-        -- default list of enabled providers defined so that you can extend it
-        -- elsewhere in your config, without redefining it, due to `opts_extend`
         sources = {
             default = { "lsp", "path", "snippets", "buffer" },
-            -- optionally disable cmdline completions
-            -- cmdline = {},
         },
 
-        -- experimental signature help support
         -- signature = { enabled = true }
     },
     -- allows extending the providers array elsewhere in your config
