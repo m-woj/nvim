b7b7ce48ae65f3630ec1fa118088a0fb0346348c
Mateusz Wojciechowski <matwojciechowski@mon.gov.pl>
2025-01-02 15:13:15 +0100

    lsp fix

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -16,12 +16,48 @@ return {
         require("mason").setup()
         require("mason-lspconfig").setup()
 
-        local lspconfig = require("lspconfig")
+        local lsp = require("lspconfig")
+
+        -- After setting up mason-lspconfig you may set up servers via lspconfig
+        lsp.lua_ls.setup({
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        -- Get the language server to recognize the `vim` global
+                        globals = { "vim" },
+                    },
+                },
+            },
+        })
+
+        lsp.pylsp.setup({
+            filetypes = { "python" },
+        })
+
+        lsp.jinja_lsp.setup({
+            filetypes = { "jinja", "htmldjango" },
+        })
+
+        lsp.ts_ls.setup({
+            filetypes = { "javascript", "typescript" },
+        })
+
+        lsp.cssls.setup({
+            filetypes = { "css", "scss", "less" },
+        })
+
+        lsp.css_variables.setup({
+            filetypes = { "css", "scss", "less" },
+        })
+
+        lsp.jsonls.setup({
+            filetypes = { "json" },
+        })
         for server, config in pairs(opts.servers) do
             -- passing config.capabilities to blink.cmp merges with the capabilities in your
             -- `opts[server].capabilities, if you've defined it
             config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
-            lspconfig[server].setup(config)
+            lsp[server].setup(config)
         end
     end,
 }
