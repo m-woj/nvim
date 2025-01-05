9f5b43c76877cd203e0a0cca0b81565712040f89
Mateusz Wojciechowski <mwojciechowski296@gmail.com>
2025-01-05 11:37:10 +0100

    session plugin changed

--- lua/plugins/session.lua
+++ lua/plugins/session.lua
@@ -1,23 +1,29 @@
 return {
-    "Shatur/neovim-session-manager",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
+    "rmagatti/auto-session",
+    lazy = false,
+
+    ---enables autocomplete for opts
+    ---@module "auto-session"
+    ---@type AutoSession.Config
+    opts = {
+        enabled = true, -- Enables/disables auto creating, saving and restoring
+        root_dir = vim.fn.stdpath("data") .. "/sessions/", -- Root dir where sessions will be stored
+        auto_save = true, -- Enables/disables auto saving session on exit
+        auto_restore = true, -- Enables/disables auto restoring session on start
+        auto_create = true, -- Enables/disables auto creating new session files. Can take a function that should return true/false if a new session file should be created or not
+        suppressed_dirs = nil, -- Suppress session restore/create in certain directories
+        allowed_dirs = nil, -- Allow session restore/create in certain directories
+        auto_restore_last_session = false, -- On startup, loads the last saved session if session for cwd does not exist
+        use_git_branch = false, -- Include git branch name in session name
+        lazy_support = true, -- Automatically detect if Lazy.nvim is being used and wait until Lazy is done to make sure session is restored correctly. Does nothing if Lazy isn't being used. Can be disabled if a problem is suspected or for debugging
+        bypass_save_filetypes = nil, -- List of filetypes to bypass auto save when the only buffer open is one of the file types listed, useful to ignore dashboards
+        close_unsupported_windows = true, -- Close windows that aren't backed by normal file before autosaving a session
+        args_allow_single_directory = true, -- Follow normal sesion save/load logic if launched with a single directory as the only argument
+        args_allow_files_auto_save = false, -- Allow saving a session even when launched with a file argument (or multiple files/dirs). It does not load any existing session first. While you can just set this to true, you probably want to set it to a function that decides when to save a session when launched with file args. See documentation for more detail
+        continue_restore_on_error = true, -- Keep loading the session even if there's an error
+        show_auto_restore_notif = false, -- Whether to show a notification when auto-restoring
+        cwd_change_handling = false, -- Follow cwd changes, saving a session before change and restoring after
+        lsp_stop_on_restore = false, -- Should language servers be stopped when restoring a session. Can also be a function that will be called if set. Not called on autorestore from startup
+        log_level = "error", -- Sets the log level of the plugin (debug, info, warn, error).
     },
-    config = function()
-        local Path = require("plenary.path")
-        local config = require("session_manager.config")
-        require("session_manager").setup({
-            sessions_dir = Path:new(vim.fn.stdpath("data"), "sessions"), -- The directory where the session files will be saved.
-            autoload_mode = config.AutoloadMode.CurrentDir, -- Define what to do when Neovim is started without arguments. See "Autoload mode" section below.
-            autosave_last_session = true, -- Automatically save last session on exit and on session switch.
-            autosave_ignore_not_normal = true, -- Plugin will not save a session when no buffers are opened, or all of them aren't writable or listed.
-            autosave_ignore_dirs = {}, -- A list of directories where the session will not be autosaved.
-            autosave_ignore_filetypes = { -- All buffers of these file types will be closed before the session is saved.
-                "gitcommit",
-                "gitrebase",
-            },
-            autosave_only_in_session = true, -- Always autosaves session. If true, only autosaves after a session is active.
-            max_path_length = 80, -- Shorten the display path if length exceeds this threshold. Use 0 if don't want to shorten the path at all.
-        })
-    end,
 }
